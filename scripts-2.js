f5554b8e453aa1540bc7d65150d050d998c21871
gjaitly <104820199+gjaitly@users.noreply.github.com>
2023-11-09 23:27:19 +0530

    Update scripts-2.js

--- scripts-2.js
+++ scripts-2.js
@@ -1,9 +1,12 @@
-
-
-
-
-
-
+<!-- Gainsight PX Tag-->
+  <script type="text/javascript">
+  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-B9GARC08ZVUL-2");
+</script>
+<!-- Gainsight PX Tag-->
 
 function validate() {
 	// var r=Math.floor(Math.random()*4);
